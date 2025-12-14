7712fc8364c64b8757908952ea44c238ae9d8e80
MOSaves <195579990+MOCodifyX@users.noreply.github.com>
2025-12-14 20:45:49 -0300

    chore(init): Commit Inicial

---
+++ volley-manager/src/main/java/com/mocodifyx/volley/VolleyManagerApplication.java
@@ -0,0 +1,13 @@
+package com.mocodifyx.volley;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolleyManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VolleyManagerApplication.class, args);
+	}
+
+}
