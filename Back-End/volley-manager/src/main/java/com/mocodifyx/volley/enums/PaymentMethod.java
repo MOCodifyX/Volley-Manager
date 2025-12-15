bdaa4fd3782ceb1531b6a1a16e5198518f4c3b66
MOSaves <195579990+MOCodifyX@users.noreply.github.com>
2025-12-14 21:15:04 -0300

    feat(enums): Criado Enum PaymentMethod

---
+++ Back-End/volley-manager/src/main/java/com/mocodifyx/volley/enums/PaymentMethod.java
@@ -0,0 +1,9 @@
+package com.mocodifyx.volley.enums;
+
+public enum PaymentMethod {
+    CREDIT_CARD,   // Cartão de crédito
+    DEBIT_CARD,    // Cartão de débito
+    PIX,           // PIX
+    BOLETO,        // Boleto bancário
+    CASH           // Dinheiro físico
+}
