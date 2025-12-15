f122f9b76312c813ee4995b94128cb9f0a3bc68e
MOSaves <195579990+MOCodifyX@users.noreply.github.com>
2025-12-14 22:00:49 -0300

    feat(enums): Criado Enum NotificationStatus

---
+++ Back-End/volley-manager/src/main/java/com/mocodifyx/volley/enums/NotificationStatus.java
@@ -0,0 +1,9 @@
+package com.mocodifyx.volley.enums;
+
+public enum NotificationStatus {
+    PENDING,    // Mensagem Pendente
+    SENT,       // Mensagem Enviada
+    READ,       // Mensagem Lida
+    FAILED      // Falha ao Enviar a Mensagem
+}
+
