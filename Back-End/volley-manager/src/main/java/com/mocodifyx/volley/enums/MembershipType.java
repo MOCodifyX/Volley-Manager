f2cefeea5cdc6baf3c6658e23d41e7aa218fc2c9
MOSaves <195579990+MOCodifyX@users.noreply.github.com>
2025-12-14 22:18:51 -0300

    feat(enums): Criado Enum MembershipType

---
+++ Back-End/volley-manager/src/main/java/com/mocodifyx/volley/enums/MembershipType.java
@@ -0,0 +1,6 @@
+package com.mocodifyx.volley.enums;
+
+public enum MembershipType {
+    SINGLE_MATCH,   // Jogador avulso, paga por partida
+    MONTHLY         // Mensalista, paga uma assinatura mensal
+}
