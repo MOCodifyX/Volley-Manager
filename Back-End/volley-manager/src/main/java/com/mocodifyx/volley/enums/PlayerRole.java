eb60e65cf141d266e5b0b9964094ffef63914116
MOSaves <195579990+MOCodifyX@users.noreply.github.com>
2025-12-14 21:37:10 -0300

    feat(enums): Criado Enum PlayerRole

---
+++ Back-End/volley-manager/src/main/java/com/mocodifyx/volley/enums/PlayerRole.java
@@ -0,0 +1,10 @@
+package com.mocodifyx.volley.enums;
+
+public enum PlayerRole {
+    SETTER,          // Levantador: responsável por organizar as jogadas e distribuir os passes
+    OUTSIDE_HITTER,  // Ponta: atacante que também ajuda na defesa e recepção
+    LIBERO,          // Líbero: especialista em defesa, não pode atacar nem sacar
+    MIDDLE_BLOCKER,  // Central: principal bloqueador e atacante rápido no meio da rede
+    OPPOSITE         // Oposto: atacante principal, atua geralmente no ataque e saque
+}
+
