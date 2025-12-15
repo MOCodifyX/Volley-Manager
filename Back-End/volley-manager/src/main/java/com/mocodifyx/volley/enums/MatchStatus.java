e01cc7b48d7fa82b72e9fb9b9214476132f41797
MOSaves <195579990+MOCodifyX@users.noreply.github.com>
2025-12-14 21:23:09 -0300

    feat(enums): Criado Enum MatchStatus

---
+++ Back-End/volley-manager/src/main/java/com/mocodifyx/volley/enums/MatchStatus.java
@@ -0,0 +1,8 @@
+package com.mocodifyx.volley.enums;
+
+public enum MatchStatus {
+    SCHEDULED,   // Partida agendada
+    IN_PROGRESS, // Partida em andamento
+    COMPLETED,   // Partida finalizada
+    CANCELED     // Partida cancelada
+}
