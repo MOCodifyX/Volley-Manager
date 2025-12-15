350fb4b706424e16ace9df49db4bc5e32d48e641
MOSaves <195579990+MOCodifyX@users.noreply.github.com>
2025-12-14 21:13:37 -0300

    feat(enums): Criado Enum PaymentStatus

---
+++ Back-End/volley-manager/src/main/java/com/mocodifyx/volley/enums/PaymentStatus.java
@@ -0,0 +1,9 @@
+package com.mocodifyx.volley.enums;
+
+public enum PaymentStatus {
+    PENDING,    // Pagamento pendente
+    OVERDUE,     // Pagamento vencido / atrasado
+    COMPLETED,  // Pagamento concluído
+    FAILED,     // Pagamento falhou
+    CANCELED    // Pagamento cancelado
+}
